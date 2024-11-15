a6c54be5256ff1426d7d013c438c3ef06776fef0
Thibaud <thibaud.lefour@eemi.com>
2024-11-15 15:17:58 +0100

    changement layout

--- src/app/page.js
+++ src/app/page.js
@@ -16,21 +16,14 @@ const router = useRouter();
       </Head>
 
       <header className={styles.header}>
-        <h1>Bienvenue sur Drive Courses !</h1>
+        <h1>Bienvenue sur le giga drive !</h1>
         <p>Vos courses en ligne, prêtes à être récupérées ou livrées en un clin d'œil.</p>
         <a href="#commencer" className={styles.button}>Commencez vos courses</a>
       </header>
 
       <main className={styles.main}>
         <section className={styles.features}>
-          <div className={styles.feature}>
-            <h2>Choisissez vos produits</h2>
-            <p>Parcourez notre large sélection de produits, de l'épicerie aux produits frais, directement en ligne.</p>
-          </div>
-        </section>
-        <section id="commencer" className={styles.callToAction}>
-          <h2>Prêt à faire vos courses ?</h2>
-          <p>Rejoignez notre service dès aujourd'hui et profitez de la simplicité d'un drive pour vos courses.</p>
+
           <button
             type="button"
             className="bg-blue-500 text-white p-2 m-4 rounded hover:bg-blue-600 transition"
