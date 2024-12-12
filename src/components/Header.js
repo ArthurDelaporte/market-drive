eb26b1c358b3f38fc6ee0476b952c9f3cd682b8c
DELAPORTE <arthur.delaporte@eemi.com>
2024-12-12 16:42:27 +0100

    Test commit signé #3
    Signed-off-by: arthurdelaporte <arthur.delaporte@eemi.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -60,7 +60,7 @@ export default function Header() {
                 <div className="flex space-x-4">
                     {/*<Link href="/panier">*/}
                         <button className="bg-white text-teal-500 px-4 py-2 rounded shadow hover:bg-gray-100 transition">
-                            Mon panier
+                            Panier
                         </button>
                     {/*</Link>*/}
                     {user ? (
