68fb26018d255d1d971444c2da390de9385dc7d8
arthurdelaporte <arthur.delaporte@eemi.com>
2024-12-12 17:05:03 +0100

    Test commit signé #5

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -30,7 +30,7 @@ export default function LogoutButton() {
             onClick={handleLogout}
             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
-            Déconnexion
+            Se déconnecter
         </button>
     );
 }
