e18022d3a7f8b27003c93d9088d00caccb967be3
Thibaud <thibaud.lefour@eemi.com>
2024-12-31 13:23:15 +0100

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import { getCookie } from "typescript-cookie";
 
 export default function Header() {
     const [user, setUser] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -42,6 +43,10 @@ export default function Header() {
     return (
         <header className="bg-teal-500 text-white p-8 rounded-b-lg shadow-md relative">
             <div className="container mx-auto flex justify-between items-center">
+            
+                <div className="flex items-center space-x-4">
+                    <img src="img/logo/Logo.png" alt="Logo GIGA Drive" className="h-20" />
+                </div>
                 {user ? (
                         <div>
                             <span className="text-2xl">Bonjour {user.firstname} {user.lastname} !</span>
@@ -51,12 +56,15 @@ export default function Header() {
                         <span className="text-2xl">Bonjour !</span>
                     </div>
                 }
-                <Link href="/" className="flex-1">
-                    <div className="text-center">
-                        <h1 className="text-4xl font-bold mb-2">Bienvenue sur le GIGA Drive !</h1>
-                        <p className="text-lg">Vos courses en ligne prêtes en un clin d'œil.</p>
-                    </div>
-                </Link>
+            <div className="flex items-center space-x-4">
+                <input
+                    type="text"
+                    placeholder="Rechercher un produit..."
+                    value={searchTerm}  // Utilise la variable d'état `searchTerm`
+                    onChange={(e) => setSearchTerm(e.target.value)} // Met à jour la valeur avec `setSearchTerm`
+                    className="w-[38vw] h-[42px] p-2 border border-gray-300 rounded"  // Personnalisation de la barre de recherche
+                />
+            </div>
                 <div className="flex space-x-4">
                     {/*<Link href="/panier">*/}
                         <button className="bg-white text-teal-500 px-4 py-2 rounded shadow hover:bg-gray-100 transition">
